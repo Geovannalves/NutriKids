8560bf86d428ea13482b4549b501a6e679539d74
Geovanna <geovannalvves@gmail.com>
2026-01-12 09:48:06 -0300

    Primeiro commit

---
+++ analise_app/gui/login.py
@@ -0,0 +1,11 @@
+import customtkinter as ctk
+
+window = ctk.CTk()
+
+window.title("NutriKids")
+window.geometry("700x400")
+window.maxsize(width=900, height=550)
+window.minsize(width=500, height=300)
+
+
+window.mainloop()
